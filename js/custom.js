00928ed678c2318ccdd91fcb3413c852fa44c8e0
ilya <imakovoz93@gmail.com>
2018-06-01 12:44:36 -0400

    set up formspree with correct email

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  var message = "";
-
-  $("#sendMessage").on("click", function() {
-      message = $("#contactform").serialize();
-      $.ajax({
-          url: "//formspree.io/imakovoz93@gmail.com",
-          method: "POST",
-          data: {message: message},
-          dataType: "json"
-      });
-      alert('Thanks for the email, we\'ll be in touch promptly.');
-      return false;
-  });
+  // var message = "";
+  //
+  // $("#sendMessage").on("click", function() {
+  //     message = $("#contactform").serialize();
+  //     $.ajax({
+  //         url: "//formspree.io/santiago@santiluna.com",
+  //         method: "POST",
+  //         data: {message: message},
+  //         dataType: "json"
+  //     });
+  //     alert('Thanks for the email, we\'ll be in touch promptly.');
+  //     return false;
+  // });
 });
